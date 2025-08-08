cc96872a29fab77d742f5b796fc72de8576497b2
danirodeyns <subtype-makko7v@icloud.com>
2025-08-08 07:58:57 +0200

    Contact

---
+++ contact-mailing.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "klantendienst@stylisso.be";
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $fullMessage = "Naam: $name\n";
+    $fullMessage .= "E-mail: $email\n";
+    $fullMessage .= "Onderwerp: $subject\n\n";
+    $fullMessage .= "Bericht:\n$message\n";
+
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        echo "Bedankt! Je bericht is verzonden.";
+    } else {
+        echo "Er is een fout opgetreden. Probeer het later opnieuw.";
+    }
+}
+?>
